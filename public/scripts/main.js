0ac8a157b0760e62ee0dd442b8916c5abcaa1f3b
João Vítor <joaovitorsf7@hotmail.com>
2021-01-25 09:34:51 -0300

    Dec2Bin.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,5 @@
-binary = null;
-
-function capture() {
-  binary = document.getElementById("binary").value;
+function Bin2Dec() {
+  const binary = document.getElementById("binary").value;
   if (binary.length == 0) {
     return 0;
   } else if (binary.length > 8) {
@@ -15,3 +13,46 @@ function capture() {
     }
   }
 }
+
+function Dec2Bin() {
+  const decimal = document.getElementById("decimal").value;
+  if (decimal.length == 0) {
+    return 0;
+  } else if (decimal.length > 8) {
+    return 0;
+  } else {
+    decimalCalc = parseInt(parseInt(decimal, 10).toString(2));
+    alert(decimalCalc);
+  }
+}
+
+function toogleToDec2Bin(e) {
+  e.preventDefault();
+  document.querySelector("title").textContent = "Dec2Bin";
+  document.getElementById("title-header-1").textContent = "Dec";
+  document.getElementById("title-header-2").textContent = "Bin";
+  document.getElementById("lbinary").setAttribute("hidden", true);
+  document.getElementById("binary").setAttribute("hidden", true);
+  document.getElementById("ldecimal").removeAttribute("hidden");
+  document.getElementById("decimal").removeAttribute("hidden");
+  document.getElementById("input-bin2dec").setAttribute("hidden", true);
+  document.getElementById("input-dec2bin").removeAttribute("hidden");
+}
+
+function toogleToDarkMode() {
+  if (document.getElementById("cbDarkMode").checked) {
+    document.body.classList = "darkmode";
+    document.querySelector("header").classList = "darkmode";
+  } else {
+    document.body.classList.remove("darkmode");
+    document.querySelector("header").classList.remove("darkmode");
+  }
+}
+
+document
+  .getElementById("btn-dec2bin")
+  .addEventListener("click", toogleToDec2Bin);
+
+document
+  .getElementById("cbDarkMode")
+  .addEventListener("click", toogleToDarkMode);
