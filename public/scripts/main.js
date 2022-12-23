26342cafd0f639cad9dd8e2663949ce120ec16cb
João Vítor <joaovitorsf7@hotmail.com>
2022-12-23 20:56:33 -0300

    refatoração, evento do teclado. BIN2DEC e DEC2BIN para "Esp.".

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,76 +1,96 @@
-const numbers = document.querySelectorAll(".numbers");
-const operations = document.querySelectorAll(".operations");
-let display = document.getElementById("display");
-const point = document.getElementById("point");
-const clear = document.getElementById("clear");
+const digits = document.querySelectorAll(".digit");
+const operations = document.querySelectorAll(".operation");
+const display = document.getElementById("display");
 
-function clicked_number(e) {
-  display.textContent += parseInt(e.target.textContent);
-}
-
-numbers[0].addEventListener("click", clicked_number);
-numbers[1].addEventListener("click", clicked_number);
-numbers[2].addEventListener("click", clicked_number);
-numbers[3].addEventListener("click", clicked_number);
-numbers[4].addEventListener("click", clicked_number);
-numbers[5].addEventListener("click", clicked_number);
-numbers[6].addEventListener("click", clicked_number);
-numbers[7].addEventListener("click", clicked_number);
-numbers[8].addEventListener("click", clicked_number);
-numbers[9].addEventListener("click", clicked_number);
-
-function clicked_operations(e) {
-  display.textContent += e.target.textContent;
-}
-
-operations[0].addEventListener("click", clicked_operations);
-operations[1].addEventListener("click", clicked_operations);
-operations[2].addEventListener("click", clicked_operations);
-
-function display_result() {
- /**
-   * for operations, the value of display.textContent is taken
-   * and the operations in this are executed e soon after
-   * are diplayed in display, if an error occurs,
-   * values are "erased".
-   */
-  try {
-    const result = Function("return ".concat(display.textContent))();
-    display.textContent = result;
-  } catch (SyntaxError) {
-    display.textContent = "#Error";
+window.addEventListener("keypress", (e) => {
+  if (e.key.match(/[0-9]/g)) {
+    display.textContent += parseInt(e.key);
   }
-  return display.textContent;
-}
 
-operations[3].addEventListener("click", display_result);
-operations[4].addEventListener("click", clicked_operations);
+  if (e.key.match(/[-*+()/.]/g)) {
+    display.textContent += e.key;
+  }
 
-// BIN2DEC
-operations[5].addEventListener("click", () => {
-  if (display.textContent.length != 0) {
-    if (isNaN(parseInt(display.textContent, 2))) {
-      display.textContent = "#Error";
-    } else {
-      display.textContent = parseInt(display.textContent, 2);
+  if (e.key == "=") {
+      try {
+        const result = Function("return ".concat(display.textContent))();
+        display.textContent = result;
+      } catch (SyntaxError) {
+        display.textContent = "#Error";
+      }
+  }
+});
+
+for (let i = 0; i < 10; i++) {
+  digits[i].addEventListener("click", (e) => {
+    display.textContent += parseInt(e.target.textContent);
+  });
+
+  digits[i].addEventListener("keypress", (e) => {
+    if (i == parseInt(e.key)) {
+      display.textContent += parseInt(e.target.textContent);
     }
+  });
+}
+
+for (let i = 10; i < 13; i++) {
+  digits[i].addEventListener("click", (e) => {
+    display.textContent += e.target.textContent;
+  })
+}
+
+for (let i = 0; i < operations.length; i++) {
+  if (i == 3) {
+    operations[i].addEventListener("click", (e) => {
+      try {
+        const result = Function("return ".concat(display.textContent))();
+        display.textContent = result;
+      } catch (SyntaxError) {
+        display.textContent = "#Error";
+      }
+    });
+  } else if (i == operations.length - 1) {
+    operations[i].addEventListener("click", (e) => {
+      display.textContent = "";
+    });
+  } else {
+    operations[i].addEventListener("click", (e) => {
+      display.textContent += e.target.textContent;
+    });
   }
-});
+}
 
-// DEC2BIN
-operations[6].addEventListener("click", () => {
-  if (display.textContent.length != 0) {
-    display.textContent = parseInt(parseInt(display.textContent, 10).toString(2));
+let ctx_menu_isActive = false;
+document.getElementById("specific").addEventListener("click", (e) => {
+  if (ctx_menu_isActive == false) {
+    e.target.innerHTML += `<div id="ctx-menu">
+<button id="bin2dec" class="operation">BIN2DEC</button>
+<button id="dec2bin" class="operation">DEC2BIN</button>
+</div>`;
+
+    const ctx_menu = document.getElementById("ctx-menu");
+    ctx_menu.style.left = `${e.clientX}px`;
+    ctx_menu.style.top = `${e.clientY}px`;
+
+    document.getElementById("bin2dec").addEventListener("click", (e) => {
+      if (display.textContent.length != 0) {
+        if (isNaN(parseInt(display.textContent, 2))) {
+          display.textContent = "#Error";
+        } else {
+          display.textContent = parseInt(display.textContent, 2);
+        }
+      }
+    });
+
+    document.getElementById("dec2bin").addEventListener("click", (e) => {
+      if (display.textContent.length != 0) {
+        display.textContent = parseInt(parseInt(display.textContent, 10).toString(2));
+      }
+    })
+    ctx_menu_isActive = true;
+  } else {
+    const ctx_menu = document.getElementById("ctx-menu");
+    ctx_menu.remove();
+    ctx_menu_isActive = false;
   }
-});
-
-operations[7].addEventListener("click", clicked_operations);
-operations[8].addEventListener("click", clicked_operations);
-
-clear.addEventListener("click", (e) => {
-  display.textContent = "";
-});
-
-point.addEventListener("click", (e) => {
-  display.textContent += ".";
 });
--- tests/tests.js
+++ tests/tests.js
@@ -5,52 +5,57 @@ window.onload = function () {
   QUnit.module("Teste de cálculo", () => {
     QUnit.test("1+1", assert => {
       display.textContent = "";
-      numbers[0].click();
+      digits[0].click();
       operations[4].click();
-      numbers[0].click();
-      assert.equal(display_result(), 2);
+      digits[0].click();
+      operations[3].click();
+      assert.equal(parseInt(display.textContent), 2);
     });
 
     QUnit.test("1-1", assert => {
       display.textContent = "";
-      numbers[0].click();
+      digits[0].click();
       operations[2].click();
-      numbers[0].click();
-      assert.equal(display_result(), 0);
+      digits[0].click();
+      operations[3].click();
+      assert.equal(parseInt(display.textContent), 0);
     });
 
     QUnit.test("1*1", assert => {
       display.textContent = "";
-      numbers[0].click();
+      digits[0].click();
       operations[1].click();
-      numbers[0].click();
-      assert.equal(display_result(), 1);
+      digits[0].click();
+      operations[3].click();
+      assert.equal(parseInt(display.textContent), 1);
     });
 
     QUnit.test("1/1", assert => {
       display.textContent = "";
-      numbers[0].click();
-      operations[0].click();
-      numbers[0].click();
-      assert.equal(display_result(), 1);
+      digits[0].click();
+      operations[1].click();
+      digits[0].click();
+      operations[3].click();
+      assert.equal(parseInt(display.textContent), 1);
     });
 
     QUnit.test("(1+2-3/4)*100", assert => {
       display.textContent = "";
-      operations[7].click();
-      numbers[0].click();
+      digits[11].click();
+      digits[0].click();
       operations[4].click();
-      numbers[1].click();
+      digits[1].click();
       operations[2].click();
-      numbers[2].click();
+      digits[2].click();
       operations[0].click();
-      numbers[3].click();
-      operations[8].click();
+      digits[3].click();
+      digits[12].click();
       operations[1].click();
-      numbers[0].click();
-      numbers[9].click();
-      numbers[9].click();
-      assert.equal(display_result(), 225);
+      digits[0].click();
+      digits[9].click();
+      digits[9].click();
+      operations[3].click();
+      assert.equal(parseInt(display.textContent), 225);
     });
   });
 };
