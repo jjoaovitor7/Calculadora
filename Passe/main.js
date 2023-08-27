90be71e5bc349f049ef1fd82a0999f01f8f47dd0
João Vítor <joaovitorsf7@hotmail.com>
2023-08-27 12:43:19 -0300

    passe - refatoração, correções e meia.

--- Passe/main.js
+++ Passe/main.js
@@ -1,35 +1,49 @@
 const checked = {
-  "seg": false, "ter": false,
-  "qua": false, "qui": false,
-  "sex": false, "sab": false,
-  "dom": false
+  seg: false,
+  ter: false,
+  qua: false,
+  qui: false,
+  sex: false,
+  sab: false,
+  dom: false,
 };
 
 const money = document.getElementById("money");
 const weeks = document.getElementById("weeks");
+const half = document.getElementById("half");
 
 let cbs_checked = [];
-
 function set_values(e) {
   if (cbs_checked.length == 0) {
     Toastify({
       text: "Selecionar pelo menos 1 caixa de seleção.",
-      duration: 3000, close: true,
-      gravity: "top", position: "right"
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "right",
     }).showToast();
   } else {
     if (e.target.id != "money") {
-      money.value = parseFloat(weeks.value)
-        * cbs_checked.length * 4.5;
+      money.value = parseFloat(weeks.value) * cbs_checked.length * 4.5;
+
+      if (half.checked) {
+        money.value = parseFloat(money.value) / 2;
+      }
     }
 
     if (e.target.id != "weeks") {
-      weeks.value = parseFloat(money.value) * ((1 / 4.5) * cbs_checked.length);
+      weeks.value = parseInt(
+        parseFloat(money.value) * ((1 / 4.5) / cbs_checked.length)
+      );
+
+      if (half.checked) {
+        weeks.value = parseFloat(weeks.value) * 2;
+      }
     }
   }
 }
 
-document.querySelectorAll("input[type='checkbox']").forEach((el) => {
+document.querySelectorAll(".week").forEach((el) => {
   el.addEventListener("click", (e) => {
     const day = el.parentElement.textContent.trim();
     const format = day.toLowerCase().substring(0, 3);
@@ -49,3 +63,10 @@ document.querySelectorAll("input[type='checkbox']").forEach((el) => {
 
 money.addEventListener("input", set_values);
 weeks.addEventListener("input", set_values);
+
+half.addEventListener("click", (el) => {
+  if (el.target.checked) {
+    money.value = parseFloat(money.value) / 2;
+    weeks.value = parseInt(weeks.value) * 2;
+  }
+});
